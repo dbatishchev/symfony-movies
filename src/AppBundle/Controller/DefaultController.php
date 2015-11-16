5a71dd225ebde583f695579cda5603e391b37ae4
dbatishchev <dimitrybatishchev@gmail.com>
2015-11-16 21:29:55 +0300

    add index page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $movies = $this->getDoctrine()
+            ->getRepository('AppBundle:Movie')->findAll();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'movies' => $movies,
         ));
     }
 }
